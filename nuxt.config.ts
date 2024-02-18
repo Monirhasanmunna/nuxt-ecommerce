0fe6301b31d0437193ed69a2d560f7ed39ca5b90
MH Munna <monirhasanmunna1@gmail.com>
2024-02-18 23:40:40 +0600

    project initial

---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: [
+    '~/assets/css/animate.css',
+    '~/assets/css/bootstrap.css',
+    '~/assets/css/flex-slider.min.css',
+    '~/assets/css/font-awesome.css',
+    '~/assets/css/jquery-ui.css',
+    '~/assets/css/magnific-popup.css',
+    '~/assets/css/magnific-popup.min.css',
+    '~/assets/css/nice-select.css',
+    '~/assets/css/niceselect.css',
+    '~/assets/css/owl-carousel.css',
+    '~/assets/css/reset.css',
+    '~/assets/css/responsive.css',
+    '~/assets/css/slicknav.min.css',
+    '~/assets/css/themify-icons.css',
+    '~/assets/css/style.css',
+  ],
+
+  app: {
+    head: {
+      link: [{rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'}]
+    }
+  }
+})
